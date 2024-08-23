b97bc387d7199545d697e7cc1db70f9ff7a14b12
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-23 23:10:29 +0100

    Added main application logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bohexists/ping-monitor-svc
+
+go 1.22.0
--- main.go
+++ main.go
@@ -1,4 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bohexists/ping-monitor-svc/workerpool"
+)
+
+const (
+	INTERVAL        = time.Minute * 1
+	REQUEST_TIMEOUT = time.Second * 1
+	WORKERS_COUNT   = 2
+)
+
+var urls = []string{
+	"https://google.com/",
+	"https://golang.org/",
+}
+
 func main() {
+	results := make(chan workerpool.Result)
+	workerPool := workerpool.New(WORKERS_COUNT, REQUEST_TIMEOUT, results)
+
+	workerPool.Init()
+
+	go generateJobs(workerPool)
+	go proccessResults(results)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	workerPool.Stop()
+}
+
+func proccessResults(results chan workerpool.Result) {
+	go func() {
+		for result := range results {
+			fmt.Println(result.Info())
+		}
+	}()
+}
+
+func generateJobs(wp *workerpool.Pool) {
+	for {
+		for _, url := range urls {
+			wp.Push(workerpool.Job{URL: url})
+		}
+
+		time.Sleep(INTERVAL)
+	}
 }
